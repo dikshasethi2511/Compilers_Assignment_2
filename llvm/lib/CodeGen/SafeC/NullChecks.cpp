3a8f05feee755166599ee2d7dfb5f7fd5039b949
Diksha Sethi <dikshasethi251@gmail.com>
2024-02-16 11:58:52 +0000

    added functionality to initialize and print the IN and OUT sets

--- llvm/lib/CodeGen/SafeC/NullChecks.cpp
+++ llvm/lib/CodeGen/SafeC/NullChecks.cpp
@@ -20,19 +20,74 @@
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 #include <deque>
+#include <unordered_map>
 
 using namespace llvm;
 
 namespace {
+
+  enum class NullCheckType {
+    UNDEFINED,
+    NOT_A_NULL,
+  };
 
 struct NullCheck : public FunctionPass {
 
   static char ID;
   NullCheck() : FunctionPass(ID) {}
 
+  // Data Flow Analysis for checking the nullpointers.
+  // Prints the IN and OUT sets for each instruction.
+  // Initialiases the IN and OUT sets for each instruction.
+  void printInOutSetsForInstructions(Function &F) {
+  ::std::unordered_map<Instruction *, NullCheckType> inSet;
+  ::std::unordered_map<Instruction *, NullCheckType> outSet;
+
+  // Initialize IN and OUT sets.
+  for (auto &B : F) {
+    for (auto &I : B) {
+      inSet[&I] = NullCheckType::UNDEFINED;
+      outSet[&I] = NullCheckType::UNDEFINED;
+    }
+  }
+
+  // Print IN and OUT sets for each instruction.
+  for (auto &B : F) {
+    for (auto &I : B) {
+      dbgs() << "Instruction: " << I << "\n";
+
+      // Print IN set.
+      dbgs() << "  IN Set: ";
+      switch (inSet[&I]) {
+      case NullCheckType::UNDEFINED:
+        dbgs() << "UNDEFINED";
+        break;
+      case NullCheckType::NOT_A_NULL:
+        dbgs() << "NOT_A_NULL";
+        break;
+      }
+      dbgs() << "\n";
+
+      // Print OUT set.
+      dbgs() << "  OUT Set: ";
+      switch (outSet[&I]) {
+      case NullCheckType::UNDEFINED:
+        dbgs() << "UNDEFINED";
+        break;
+      case NullCheckType::NOT_A_NULL:
+        dbgs() << "NOT_A_NULL";
+        break;
+      }
+      dbgs() << "\n\n";
+    }
+  }
+}
+
+
 
   bool runOnFunction(Function &F) override {
 		dbgs() << "running nullcheck pass on: " << F.getName() << "\n";
+    printInOutSetsForInstructions(F);
     return false;
   }
 }; // end of struct NullCheck
