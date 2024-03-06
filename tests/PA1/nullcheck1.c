b3301281e89f611981172fe17330abf723363bfc
Diksha Sethi <dikshasethi251@gmail.com>
2024-03-06 08:20:54 +0000

    fixed the makefile and test case

--- tests/PA1/nullcheck1.c
+++ tests/PA1/nullcheck1.c
@@ -1,30 +1,25 @@
-#include "support.h"
 #include <stdio.h>
+#include "support.h"
 
-void foo(int *arr) {
-  int *ptr = mymalloc(4);
+void foo (int *arr)
+{
+	int *ptr = mymalloc(4);
 
-  ptr[0] = 100;
-  ptr = arr;
-  // if (ptr == NULL) {
-  //   exit(0);
-  // }
-  // TODO: Fix test back to original
-  ptr[0] = 100;
-  printf('Here2 : ');
-
-  //   if (ptr == NULL) {
-  //     ptr = mymalloc(4);
-  //     ptr[0] = 100;
-  // 	}
-  // 	else {
-  // 		ptr[0] = 100;
-  // 	}
-  // 	ptr[0] = 100;
+	ptr[0] = 100;
+	ptr = arr;
+	ptr[0] = 100;
+	if (ptr == NULL) {
+		ptr = mymalloc(4);
+		ptr[0] = 100;
+	}
+	else {
+		ptr[0] = 100;
+	}
+	ptr[0] = 100;
 }
 
-int main() {
-  int *ptr = mymalloc(4);
-  foo(NULL);
-  return 0;
+int main()
+{
+	foo(NULL);
+	return 0;
 }
